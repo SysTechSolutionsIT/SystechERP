6ca4a1c555d1ea5dd5966a0254f55e795bd2a536
Divya5003 <divyamahajan5003@gmail.com>
2023-08-12 17:53:59 +0530

    sidebar fixed

--- src/App.js
+++ src/App.js
@@ -2,14 +2,30 @@ import React from "react";
 import Header from "./components/Header";
 import Sidebar from "./components/Sidebar";
 import EMPTabs from "./components/employee/emptabs";
+import { BrowserRouter as Router, Routes, Route, Outlet } from 'react-router-dom'
 
 function App() {
   return (
-    <div>
-      <Sidebar/>
-      <Header/>
-      <EMPTabs/>
-    </div>
+    // <div>
+    //   <Sidebar/>
+    //   <Header/>
+    //   <EMPTabs/>
+    // </div>
+    <Router>
+      <Routes>
+        <Route element={
+          <div className="flex">
+            <Sidebar />
+            <main className="flex-1 mx-auto">
+              <Header />
+              <div className="bg-gray-50 h-[calc(100vh - 56px)]"><Outlet /></div>
+            </main>
+          </div>
+        }>
+          <Route path='/' element={<EMPTabs />} />
+        </Route>
+      </Routes>
+    </Router>
   );
 }
 
