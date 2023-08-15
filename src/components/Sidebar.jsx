4c73a2a400bc6e5f8f0c959c3cf610fd3328b04c
Udayan Gaikwad <udayanfg@gmail.com>
2023-08-15 10:51:26 +0530

    sidebar changes, submenus added

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -4,24 +4,46 @@ import { Icon } from '@iconify/react';
 const Sidebar = () => {
 
     const [isSubMenuOpen, setSubMenuOpen] = useState(false);
+    const [isCompanySubmenuOpen, setCompanySubmenuOpen] = useState(false)
+    const [isEmployeeSubmenuOpen, setEmployeeSubmenuOpen] = useState(false)
+    const [isAttendanceSubmenuOpen, setAttendanceSubmenuOpen] = useState(false)
+    const [isAttMgtSubmenuOpen, setAttMgtSubmenuOpen] = useState(false)
+    const [isPayrollSubmenuOpen, setPayrollSubmenuOpen] = useState(false)
+    const [isLeaveSubmenuOpen, setLeaveSubmenuOpen] = useState(false)
+    const [isSalarySubmenuOpen, setSalarySubmenuOpen] = useState(false)
+    const [isRegisterSubmenuOpen, setRegisterSubmenuOpen] = useState(false)
     const [open, setOpen] = useState(true)
 
-    const toggleSubMenu = () => {
-        setSubMenuOpen(prevState => !prevState);
-    }
+    const SubMenuMain = ({ title, isOpen = false, onClick }) => (
+        <span className={`flex justify-between w-full items-center hover:bg-gray-300 hover:bg-opacity-25 rounded-lg cursor-pointer ${isOpen ? 'mb-1' : ''}`} onClick={onClick}>
+          <h1 className="font-[Inter] text-white font-semibold text-[15px] cursor-pointer py-2 px-2 rounded-md mt-1 whitespace-normal">{title}</h1>
+          <Icon icon="iconamoon:arrow-up-2" color="white" width="30" height="30" rotate={isOpen ? '' : 2} />
+        </span>
+      );
+      
+      const SubMenuGroup = ({ children }) => (
+        <div className="leading-7 text-left text-sm font-thin mt-2 px-2 w-full ease-in-out duration-200 whitespace-nowrap">
+          {children}
+        </div>
+      );
+
+    const SubMenuEntry = ({title, isOpen = false, onClick}) =>(
+        <span className='flex justify-between w-full items-center hover:bg-gray-300 hover:bg-opacity-25 rounded-lg cursor-pointer'>
+        <h1 className="font-[Inter] text-white text-[15px] cursor-pointer py-2 px-3 items-center rounded-md mt-1 whitespace-normal">{title}</h1>
+        </span>
+    )
 
     return (
         <div className='flex'>
-            <div className={`${open ? 'w-[250px]' : 'w-[85px] overflow-hidden'} bg-blue-900 sidebar bottom-0 lg:left-0 p-2 overflow-y-hidden overflow-x-hidden text-center border-0 border-radius-xl ease-in-out duration-300`}>
-                <div className='text-white-100 text-xl fixed'>
+            <div className={`${open ? 'w-[250px]' : 'w-[85px]'} bg-blue-900 overflow-auto sidebar bottom-0 lg:left-0 p-2 text-center border-0 border-radius-xl ease-in-out duration-300`}>
+                <div className='text-white-100 text-xl'>
                     <div className='p-2.5 mt-1 flex items-center'>
                         <img src='/apple-icon.png' alt='icon' />
                         {open && (
-                            <h1 className='font-[Inter] font-bold text-white justify-center text-[20px] ml-3 whitespace-nowrap'>Systech ERP</h1>
+                            <h1 className='font-[Inter] font-bold text-white justify-center text-[20px] ml-3 whitespace-nowrap'>SysTech ERP</h1>
                         )}
                     </div>
                     <hr className='text-gray-600 my-2' />
-
                     <div>
                         <div className='p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-gray-300 hover:bg-opacity-25 text-white'>
                             <Icon icon="ic:round-dashboard" color="white" width="30" height="30" />
@@ -30,12 +52,21 @@ const Sidebar = () => {
                                     <span className='font-[Inter] font-semibold text-[15px] ml-4 text-white-200'>Dashboard</span>
                                 </div>)}
                         </div>
+                        
+                        <div className='p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-gray-300 hover:bg-opacity-25 text-white'>
+                            <Icon icon="akar-icons:person-add" color="white" width="30" height="30" />
+                            {open && (
+                                <div className="flex justify-between w-full items-center">
+                                    <span className='font-[Inter] font-semibold text-[15px] ml-4 text-white-200'>User Settings</span>
+                                </div>)}
+                        </div>
+                        
 
                         <div className='p-2.5 mt-3 flex items-center rounded-md px-4 duration-300
             cursor-pointer hover:bg-gray-300 hover:bg-opacity-25 text-white'>
                             <Icon icon="clarity:employee-group-line" color="white" width="30" height="30" />
                             {open && (
-                                <div className="flex justify-between w-full items-center" onClick={toggleSubMenu}>
+                                <div className="flex justify-between w-full items-center" onClick={()=> setSubMenuOpen(prevState => !prevState)}>
                                     <span className='font-[Inter] font-semibold text-[15px] ml-4 text-white-200'>HRM</span>
                                     <span className={`text-sm ${isSubMenuOpen ? '' : 'rotate-180'} ease-linear duration-200`} id="arrow">
                                         <Icon icon="iconamoon:arrow-up-2" color="white" width="30" height="30" />
@@ -44,12 +75,102 @@ const Sidebar = () => {
                             )}
                         </div>
                         {isSubMenuOpen && (
-                            <div className=" leading-7 text-left text-sm font-thin mt-2 w-4/5 mx-auto ease-linear duration-200" id="submenu">
-                                <h1 className="font-[Inter] text-white font-semibold text-[15px] cursor-pointer p-2 hover:bg-gray-300 hover:bg-opacity-25 rounded-md mt-1">Employee List</h1>
-                                <h1 className="font-[Inter] text-white font-semibold text-[15px] cursor-pointer p-2 hover:bg-gray-300 hover:bg-opacity-25 rounded-md mt-1">Attendance</h1>
-                                <h1 className="font-[Inter] text-white font-semibold text-[15px] cursor-pointer p-2 hover:bg-gray-300 hover:bg-opacity-25 rounded-md mt-1">Compliances</h1>
+                            <div className="leading-7 text-left text-sm font-thin mt-2 px-2 w-full ease-in-out duration-200 whitespace-nowrap" id="submenu">
+                                <SubMenuMain title="Company Settings" isOpen={isCompanySubmenuOpen} onClick={() => setCompanySubmenuOpen(prevState => !prevState)} />
+                                {isCompanySubmenuOpen && (
+                                <SubMenuGroup>
+                                    <SubMenuEntry title="Company Master" />
+                                    <SubMenuEntry title="Financial Year Master" />
+                                    <SubMenuEntry title="Company Configuration" />
+                                    <SubMenuEntry title="Bank Master" />
+                                    <SubMenuEntry title="Cost Center Master" />
+                                    <SubMenuEntry title="Department Master" />
+                                    <SubMenuEntry title="Destination Master" />
+                                    <SubMenuEntry title="Three Fields Master" />
+                                    <SubMenuEntry title="Two Fields Master" />
+                                    <SubMenuEntry title="Project Master" />
+                                </SubMenuGroup>
+                                )}
+                                <SubMenuMain title="Employee Settings" isOpen={isEmployeeSubmenuOpen} onClick={()=> setEmployeeSubmenuOpen(prevState => !prevState)} />
+                                {isEmployeeSubmenuOpen && (
+                                <SubMenuGroup>
+                                    <SubMenuEntry title="Employee Master" />
+                                    <SubMenuEntry title="Employee Type Master" />
+                                    <SubMenuEntry title="Employee Grade Master" />
+                                    <SubMenuEntry title="Designation Master" />
+                                    <SubMenuEntry title="KRA Master" />
+                                    <SubMenuEntry title="Job Responsibility Master" />
+                                    <SubMenuEntry title="Employee Band Master" />
+                                </SubMenuGroup>
+                                )}
+                                <SubMenuMain title="Attendance Settings" isOpen={isAttendanceSubmenuOpen} onClick={()=>setAttendanceSubmenuOpen(prevState => !prevState)} />
+                                {isAttendanceSubmenuOpen &&(
+                                <SubMenuGroup>
+                                    <SubMenuEntry title="Job Type Master" />
+                                    <SubMenuEntry title="Shift Master" />
+                                    <SubMenuEntry title="Weekly Off Master" />
+                                    <SubMenuEntry title="Holiday Master" />
+                                    <SubMenuEntry title="Atten. Device Master" />
+                                </SubMenuGroup>
+                                )}
+                                <SubMenuMain title="Payroll Management" isOpen={isPayrollSubmenuOpen} onClick={()=> setPayrollSubmenuOpen(prevState => !prevState)}/>
+                                {isPayrollSubmenuOpen && (
+                                <SubMenuGroup>
+                                    <SubMenuEntry title="Earning Heads Master" />
+                                    <SubMenuEntry title="Deduction Heads Master" />
+                                    <SubMenuEntry title="Employee Type Earning Deduciton" />
+                                    <SubMenuEntry title="Professional Tax Setting" />
+                                    <SubMenuEntry title="Advance Request" />
+                                    <SubMenuEntry title="Advance Approval"/>
+                                    <SubMenuEntry title="Advance Repayment"/>
+                                </SubMenuGroup>
+                                )}
+                                <SubMenuMain title="Leaves Management" isOpen={isLeaveSubmenuOpen} onClick={()=> setLeaveSubmenuOpen(prevState => !prevState)} />
+                                {isLeaveSubmenuOpen && (
+                                <SubMenuGroup>
+                                   <SubMenuEntry title="Leave Type Master" />
+                                   <SubMenuEntry title="Leave Balance Upload" />
+                                   <SubMenuEntry title="Leave Application" />
+                                   <SubMenuEntry title="Leave Approvals" />
+                                </SubMenuGroup> 
+                                )}
+                                <SubMenuMain title="Attendance Management" isOpen={isAttMgtSubmenuOpen} onClick={()=> setAttMgtSubmenuOpen(prevState => !prevState)}/>
+                                {isAttMgtSubmenuOpen && (
+                                <SubMenuGroup>
+                                    <SubMenuEntry title="Shift roster" />
+                                    <SubMenuEntry title="Mnual Attendance Entry" />
+                                    <SubMenuEntry title="Manual Attendance Approval" />
+                                    <SubMenuEntry title="Out Door Duty Attendance Entry " />
+                                    <SubMenuEntry title="Out Door Duty Attendance Application" />
+                                    <SubMenuEntry title="Employee Gate Pass Entry"/>
+                                    <SubMenuEntry title="Employee Gate Pass Approval"/>
+                                    <SubMenuEntry title="Job Allocation"/>
+                                    <SubMenuEntry title="Daily Atttendance Processing"/>
+                                    <SubMenuEntry title="Attendance Import"/>
+                                    <SubMenuEntry title="Monthly Attendance Provessing"/>
+                                </SubMenuGroup>
+                                )}
+                                <SubMenuMain title="Salary Management" isOpen={isSalarySubmenuOpen} onClick={()=> setSalarySubmenuOpen(prevState => !prevState)}/>
+                                {isSalarySubmenuOpen && (
+                                <SubMenuGroup>
+                                    <SubMenuEntry title="Daily Overtime Processing" />
+                                    <SubMenuEntry title="Monthly Overtime Processing" />
+                                    <SubMenuEntry title="Overtime Approvals" />
+                                    <SubMenuEntry title="Advance Management " />
+                                    <SubMenuEntry title="Earning-Deduction Imports" />
+                                    <SubMenuEntry title="Salary Processing"/>
+                                    <SubMenuEntry title="Salary Corrections"/>
+                                </SubMenuGroup>
+                                )}
+                                <SubMenuMain title="Registers" isOpen={isRegisterSubmenuOpen} onClick={()=> setRegisterSubmenuOpen(prevState => !prevState)} />
+                                    {isRegisterSubmenuOpen && (
+                                    <SubMenuGroup>
+                                        <SubMenuEntry title="Master Register" />
+                                    </SubMenuGroup>
+                                    )}
                             </div>
-                        )}
+                            )}
+
 
                         <div className='p-2.5 mt-3 flex items-center rounded-md px-4 duration-300
             cursor-pointer hover:bg-gray-300 hover:bg-opacity-25 text-white'>
