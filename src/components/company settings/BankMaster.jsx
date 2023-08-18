53a8d8cac25f8dba945397f8f23dcc9a93fd5691
Mukta Patil <muktap2002@gmail.com>
2023-08-18 23:05:08 +0530

    column visibility

--- src/components/company settings/BankMaster.jsx
+++ src/components/company settings/BankMaster.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Icon } from "@iconify/react";
 import BankModal from "./BankModal";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export const bankData = [
   {
@@ -87,6 +87,14 @@ export const bankData = [
 ];
 
 const BankMaster = () => {
+  const [columnVisibility, setColumnVisibility] = useState({
+    bankName: true,
+    branchName: true,
+    accountType: true,
+    accountNo: true,
+    ifscCode: true,
+  });
+
   const [isModalOpen, setModalOpen] = useState(false);
   const handleModalClose = () => {
     setModalOpen(false);
@@ -94,7 +102,6 @@ const BankMaster = () => {
   const [filteredData, setFilteredData] = useState([]);
 
   const handleSearchChange = (title, searchWord) => {
-    const newFilter = bankData.filter((item) => {
     const newFilter = bankData.filter((item) => {
       const value = item[title];
       return value && value.toLowerCase().includes(searchWord.toLowerCase());
@@ -105,6 +112,40 @@ const BankMaster = () => {
     } else {
       setFilteredData(newFilter);
     }
+  };
+
+  const toggleColumnVisibility = (column) => {
+    setColumnVisibility((prevVisibility) => ({
+      ...prevVisibility,
+      [column]: !prevVisibility[column],
+    }));
+  };
+
+  const [showDropdown, setShowDropdown] = useState(false);
+  const [selectedColumns, setSelectedColumns] = useState([
+    ...Object.keys(columnVisibility),
+  ]);
+
+  const toggleColumn = (columnName) => {
+    if (selectedColumns.includes(columnName)) {
+      setSelectedColumns((prevSelected) =>
+        prevSelected.filter((col) => col !== columnName)
+      );
+    } else {
+      setSelectedColumns((prevSelected) => [...prevSelected, columnName]);
+    }
+  };
+
+  useEffect(() => {
+    console.log("Selected Columns:", selectedColumns);
+  }, [selectedColumns]);
+
+  const selectAllColumns = () => {
+    setSelectedColumns([...Object.keys(columnVisibility)]);
+  };
+
+  const deselectAllColumns = () => {
+    setSelectedColumns([]);
   };
   return (
     <div className="p-8">
@@ -128,6 +169,58 @@ const BankMaster = () => {
           <button className="bg-white text-blue-900 border border-blue-900 font-semibold py-2 px-4 rounded-lg">
             Print
           </button>
+          <div className="relative">
+            <button
+              onClick={() => setShowDropdown(!showDropdown)}
+              className="flex bg-white text-blue-900 border border-blue-900 hover:bg-blue-900 hover:text-white duration-200 font-semibold py-2 px-4 rounded-lg"
+            >
+              Column Visibility
+              <Icon
+                icon="fe:arrow-down"
+                className={`mt-1.5 ml-2 ${showDropdown ? "rotate-180" : ""}`}
+              />
+            </button>
+            {showDropdown && (
+              <div className="absolute top-10 right-0 bg-white border border-gray-300 shadow-md rounded-lg p-2">
+                <div className="flex items-center mb-2">
+                  <button
+                    className="text-blue-500 hover:text-blue-700 underline mr-2"
+                    onClick={selectAllColumns}
+                  >
+                    Select All
+                  </button>
+                  <button
+                    className="text-blue-500 hover:text-blue-700 underline"
+                    onClick={deselectAllColumns}
+                  >
+                    Deselect All
+                  </button>
+                </div>
+                {Object.keys(columnVisibility).map((columnName) => (
+                  <label
+                    key={columnName}
+                    className="flex items-center capitalize"
+                  >
+                    <input
+                      type="checkbox"
+                      className="mr-2 "
+                      checked={selectedColumns.includes(columnName)}
+                      onChange={() => toggleColumn(columnName)}
+                    />
+                    <span
+                      className={
+                        selectedColumns.includes(columnName)
+                          ? "font-semibold"
+                          : ""
+                      }
+                    >
+                      {columnName}
+                    </span>
+                  </label>
+                ))}
+              </div>
+            )}
+          </div>
         </div>
         <button
           className="bg-blue-900 text-white font-semibold py-2 px-4 rounded-lg mr-20"
@@ -148,21 +241,16 @@ const BankMaster = () => {
                 <th className="px-1 font-semibold text-black border-2 border-gray-400">
                   Bank ID
                 </th>
-                <th className="px-1 font-semibold text-black border-2 border-gray-400">
-                  Bank Name
-                </th>
-                <th className="px-1 font-semibold text-black border-2 border-gray-400">
-                  Branch Name
-                </th>
-                <th className="px-1 font-semibold text-black border-2 border-gray-400">
-                  Account Type
-                </th>
-                <th className="px-1 font-semibold text-black border-2 border-gray-400">
-                  Account No
-                </th>
-                <th className="px-1 font-semibold text-black border-2 border-gray-400">
-                  IFSC Code
-                </th>
+                {selectedColumns.map((columnName) => (
+                  <th
+                    key={columnName}
+                    className={`px-1 font-semibold text-black border-2 border-gray-400 ${
+                      columnVisibility[columnName] ? "" : "hidden"
+                    }`}
+                  >
+                    {columnName}
+                  </th>
+                ))}
               </tr>
               <tr>
                 <th className="border-2"></th>
@@ -176,104 +264,64 @@ const BankMaster = () => {
                     }
                   />
                 </th>
-                <th className="p-2 font-semibold text-black border-2">
-                  <input
-                    type="text"
-                    placeholder="Search"
-                    className="w-32 border-2 border-slate-500 rounded-lg justify-center text-center"
-                    onChange={(e) =>
-                      handleSearchChange("bankName", e.target.value)
-                    }
-                  />
-                </th>
-                <th className="p-2 font-semibold text-black border-2">
-                  <input
-                    type="text"
-                    placeholder="Search"
-                    className="w-32 border-2 border-slate-500 rounded-lg justify-center text-center"
-                    onChange={(e) =>
-                      handleSearchChange("branchName", e.target.value)
-                    }
-                  />
-                </th>
-                <th className="p-2 font-semibold text-black border-2">
-                  <input
-                    type="text"
-                    placeholder="Search"
-                    className="w-32  border-2 border-slate-500 rounded-lg justify-center text-center"
-                    onChange={(e) =>
-                      handleSearchChange("accountType", e.target.value)
-                    }
-                  />
-                </th>
-                <th className="p-2 font-semibold text-black border-2">
-                  <input
-                    type="text"
-                    placeholder="Search"
-                    className="w-32 border-2 border-slate-500 rounded-lg justify-center text-center"
-                    onChange={(e) =>
-                      handleSearchChange("accountNo", e.target.value)
-                    }
-                  />
-                </th>
-                <th className="p-2 font-semibold text-black border-2">
-                  <input
-                    type="text"
-                    placeholder="Search"
-                    className="w-32 border-2 border-slate-500 rounded-lg justify-center text-center"
-                    onChange={(e) =>
-                      handleSearchChange("ifscCode", e.target.value)
-                    }
-                  />
-                </th>
+                {selectedColumns.map((columnName) => (
+                  <th
+                    key={columnName}
+                    className="p-2 font-semibold text-black border-2"
+                  >
+                    <input
+                      type="text"
+                      placeholder={`Search `}
+                      className="w-32 border-2 border-slate-500 rounded-lg justify-center text-center"
+                      onChange={(e) =>
+                        handleSearchChange(columnName, e.target.value)
+                      }
+                    />
+                  </th>
+                ))}
               </tr>
             </thead>
             <tbody className="">
               {filteredData.length > 0
                 ? filteredData.map((result, key) => (
-                  <tr key={key}>
-                    <td className="px-2 border-2">
-                      <div className="flex items-center gap-2 text-center justify-center">
-                        <Icon
-                          icon="lucide:eye"
-                          color="#556987"
-                          width="20"
-                          height="20"
-                        />
-                        <Icon
-                          icon="mdi:edit"
-                          color="#556987"
-                          width="20"
-                          height="20"
-                        />
-                        <Icon
-                          icon="material-symbols:delete-outline"
-                          color="#556987"
-                          width="20"
-                          height="20"
-                        />
-                      </div>
-                    </td>
-                    <td className="px-4 border-2 whitespace-normal text-center">
-                      {result.bankId}
-                    </td>
-                    <td className="px-4 border-2 whitespace-normal text-left">
-                      {result.bankName}
-                    </td>
-                    <td className="px-4 border-2 whitespace-normal text-left">
-                      {result.branchName}
-                    </td>
-                    <td className="px-4 border-2 whitespace-normal text-left">
-                      {result.accountType}
-                    </td>
-                    <td className="px-4 border-2 whitespace-normal text-left">
-                      {result.accountNo}
-                    </td>
-                    <td className="px-4 border-2 whitespace-normal text-left">
-                      {result.ifscCode}
-                    </td>
-                  </tr>
-                ))
+                    <tr key={key}>
+                      <td className="px-2 border-2">
+                        <div className="flex items-center gap-2 text-center justify-center">
+                          <Icon
+                            icon="lucide:eye"
+                            color="#556987"
+                            width="20"
+                            height="20"
+                          />
+                          <Icon
+                            icon="mdi:edit"
+                            color="#556987"
+                            width="20"
+                            height="20"
+                          />
+                          <Icon
+                            icon="material-symbols:delete-outline"
+                            color="#556987"
+                            width="20"
+                            height="20"
+                          />
+                        </div>
+                      </td>
+                      <td className="px-4 border-2 whitespace-normal text-center">
+                        {result.bankId}
+                      </td>
+                      {selectedColumns.map((columnName) => (
+                        <td
+                          key={columnName}
+                          className={`px-4 border-2 whitespace-normal text-left${
+                            columnVisibility[columnName] ? "" : "hidden"
+                          }`}
+                        >
+                          {result[columnName]}
+                        </td>
+                      ))}
+                    </tr>
+                  ))
                 : bankData.map((entry, index) => (
                     <tr key={index}>
                       <td className="border-2">
@@ -301,21 +349,16 @@ const BankMaster = () => {
                       <td className="px-4 border-2 whitespace-normal text-center">
                         {entry.bankId}
                       </td>
-                      <td className="px-4 border-2 whitespace-normal text-left">
-                        {entry.bankName}
-                      </td>
-                      <td className="px-4 border-2 whitespace-normal text-left">
-                        {entry.branchName}
-                      </td>
-                      <td className="px-4 border-2 whitespace-normal text-left">
-                        {entry.accountType}
-                      </td>
-                      <td className="px-4 border-2 whitespace-normal text-left">
-                        {entry.accountNo}
-                      </td>
-                      <td className="px-4 border-2 whitespace-normal text-left">
-                        {entry.ifscCode}
-                      </td>
+                      {selectedColumns.map((columnName) => (
+                        <td
+                          key={columnName}
+                          className={`px-4 border-2 whitespace-normal text-left${
+                            columnVisibility[columnName] ? "" : "hidden"
+                          }`}
+                        >
+                          {entry[columnName]}
+                        </td>
+                      ))}
                     </tr>
                   ))}
             </tbody>
