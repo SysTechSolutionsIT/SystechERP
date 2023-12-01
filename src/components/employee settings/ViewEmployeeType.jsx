cf9590a9b9c806f55c1fdd9d32f3e8d15b0a18b8
Udayan Gaikwad <udayanfg@gmail.com>
2023-12-01 09:30:54 +0530

    Employee type master done

--- src/components/employee settings/ViewEmployeeType.jsx
+++ src/components/employee settings/ViewEmployeeType.jsx
@@ -21,21 +21,22 @@ const ViewEmployeeType = ({ visible, onClick, edit, ID }) => {
 
     const formik = useFormik({
         initialValues: {
+            EmployeeTypeId:"",
             EmployeeType: "",
             EmployeeTypeGroup: "",
             ShortName: "",
-            Status: "",
             Remark: ""
         },
         onSubmit: (values) => {
             console.log(values);
 
             const updatedData = {
+                EmployeeTypeId: values.EmployeeTypeId,
                 EmployeeType: values.EmployeeType,
                 EmployeeTypeGroup: values.EmployeeTypeGroup,
                 ShortName: values.ShortName,
-                Status: values.Status,
-                Remark: values.Remark
+                Remark: values.Remark,
+                IUFlag:"U"
             }
             updateEmpType(updatedData)
         },
@@ -43,10 +44,9 @@ const ViewEmployeeType = ({ visible, onClick, edit, ID }) => {
 
     const updateEmpType = async(data) =>{
         try{
-            const response = axios.patch(`http://localhost:5500/employee-type/update/${ID}`, data, {
-                headers:{
-                    Authorization: `Bearer ${token}`
-                }
+            const response = axios.post(`http://localhost:5500/employee-type/FnAddUpdateDeleteRecord`, data, {
+                params:{EmployeeTypeId : ID},
+                headers: { Authorization: `Bearer ${token}` },
             })
             alert('Employee Type Updated')
         } catch(error){
@@ -57,10 +57,9 @@ const ViewEmployeeType = ({ visible, onClick, edit, ID }) => {
     useEffect(() =>{
         const fetchEmpTypeData = async() =>{
           try{
-            const response = await axios.get(`http://localhost:5500/employee-type/get/${ID}`, {
-                headers:{
-                  Authorization: `Bearer ${token}`
-                }
+            const response = await axios.get(`http://localhost:5500/employee-type/FnShowParticularData`, {
+                params:{EmployeeTypeId : ID},
+                headers: { Authorization: `Bearer ${token}` },
               })
             const data = response.data
             console.log(data)
@@ -75,10 +74,10 @@ const ViewEmployeeType = ({ visible, onClick, edit, ID }) => {
     useEffect(() => {
         if (details){
             formik.setValues({
+                EmployeeTypeId: details.EmployeeTypeId,
                 EmployeeType: details.EmployeeType,
                 EmployeeTypeGroup: details.EmployeeTypeGroup,
                 ShortName: details.ShortName,
-                Status: details.Status,
                 Remark: details.Remark
             })
         }
@@ -107,10 +106,10 @@ const ViewEmployeeType = ({ visible, onClick, edit, ID }) => {
                                 <input
                                     type="number"
                                     placeholder="Enter Employee Type ID"
-                                    value={details?.id}
+                                    value={details?.EmployeeTypeId}
                                     className={`w-full px-4 py-2 font-normal focus:outline-blue-900 border-gray-300 border rounded-lg text-[11px] `}
                                     onChange={formik.handleChange}
-                                    disabled={!edit}
+                                    disabled={true}
                                 />
                             </div>
                             <div>
@@ -134,6 +133,7 @@ const ViewEmployeeType = ({ visible, onClick, edit, ID }) => {
                                     onChange={formik.handleChange}
                                     disabled={!edit}
                                 >
+                                    <option value={formik.values.EmployeeTypeGroup}>{formik.values.EmployeeTypeGroup}</option>
                                     <option value="">Select Employee Type Group</option>
                                     <option value="Worker">Worker</option>
                                     <option value="Staff">Staff</option>
@@ -154,7 +154,7 @@ const ViewEmployeeType = ({ visible, onClick, edit, ID }) => {
                             <div>
                                 <p className="text-[13px] text-left py-1 font-semibold">Remarks</p>
                                 <input
-                                    id="remark"
+                                    id="Remark"
                                     type="text"
                                     placeholder="Enter Remarks"
                                     value={formik.values.Remark}
@@ -163,20 +163,6 @@ const ViewEmployeeType = ({ visible, onClick, edit, ID }) => {
                                     disabled={!edit}
                                 />
                             </div>
-                            <div>
-                            <p className="capitalize font-semibold text-[13px]">Status</p>
-                            <label className="capitalize font-semibold text-[11px]">
-                            <input
-                                id="Status"
-                                type="checkbox"
-                                checked={formik.values.Status}
-                                value={formik.values.Status}
-                                className={`w-5 h-5 mr-2 mt-5 focus:outline-gray-300 border-2 rounded-lg`}
-                                onChange={(event) => handleCheckboxChange('Status', setStatusChecked, event)}
-                            />
-                            Active
-                            </label>
-                        </div>
                         </div>
                     </div>
                     <div className="flex gap-10 justify-center">
